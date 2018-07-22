275e9759a8c503a540728690a72d8dcc49ed7799
Thoriq Firdaus <thoriqoe@gmail.com>
2018-07-22 23:11:37 +0700

    Remove parameters
    The parameters are now set as the default in up.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ source "$(dirname "$0")/shared.sh"
 
 export WP_LOCKER_START=1
 
-"$(dirname "$0")"/up.sh --build -d wordpress
+"$(dirname "$0")"/up.sh
 
 while ! dexec cat wp-config.php > /dev/null 2>&1; do
 	sleep 2
